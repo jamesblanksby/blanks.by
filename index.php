17eeccb16f640fb03258eb9a33f752f97599410d
James Blanksby <james@thisismyengine.com>
2020-09-21 10:08:01 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<html lang="en">
+<head>
+    <!-- encoding -->
+    <meta charset="utf-8">
+    <!-- viewport -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- title -->
+    <title>Blanksby</title>
+    <!-- style -->
+    <link href="./src/css/site.css" rel="stylesheet">
+</head>
+<body>
+
+    <main>
+        <h1>James Blanksby</h1>
+        <p>Full Stack Developer</p>
+    </main>
+
+</body>
+</html>
