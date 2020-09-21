410990838b6054b3592d608c15b4804c02694a7e
James Blanksby <james@thisismyengine.com>
2020-09-21 10:15:36 +0100

    Added site favicon

--- index.php
+++ index.php
@@ -6,6 +6,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- title -->
     <title>Blanksby</title>
+    <!-- favicon -->
+	<?php foreach ([32, 128, 152, 167, 180, 192, 196,] as $size) : ?>
+		<link href="<?= './src/gfx/favicon/' . $size . '.' . 'png'; ?>" sizes="<?= implode('x', [$size, $size,]); ?>" rel="icon">
+	<?php endforeach; ?>
     <!-- style -->
     <link href="./src/css/site.css" rel="stylesheet">
 </head>
